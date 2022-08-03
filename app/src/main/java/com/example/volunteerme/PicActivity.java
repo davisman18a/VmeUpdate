69f752bd7bc6712c6d0dfe7ecebc6aec643d5dc4
davisman18a <aaron99davis@gmail.com>
2022-08-03 15:58:57 +0600

    Update

---
+++ app/src/main/java/com/example/volunteerme/PicActivity.java
@@ -0,0 +1,49 @@
+package com.example.volunteerme;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.analytics.FirebaseAnalytics;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class PicActivity extends AppCompatActivity {
+
+    private ImageAdapter adapter;
+    private FirebaseAnalytics mFirebaseAnalytics;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
+
+        RecyclerView rv = findViewById(R.id.images);
+        adapter = new ImageAdapter();
+        rv.setHasFixedSize(false);
+        RecyclerView.LayoutManager manager = new GridLayoutManager(getApplicationContext(),4);
+        rv.setLayoutManager(manager);
+        rv.setAdapter(adapter);
+
+
+
+
+    }
+
+
+    }
